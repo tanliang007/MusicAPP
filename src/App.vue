b346600cf1881f0771f71c5e4e60c5b225d66398
谭亮 <tan.liang@ejlchina.com>
2017-12-27 15:01:07 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+      1312123
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+@import  "common/stylus/variable"
+#app
+  color:$color-theme;
+  
+</style>
